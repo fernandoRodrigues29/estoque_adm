5008083d2975ffd593f315953610a7363028e180
fernandoRodrigues29 <fernando1587@gmail.com>
2023-06-12 15:27:18 -0300

    primeiro

---
+++ js/about.3c133e75.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkfront_estoque"]=self["webpackChunkfront_estoque"]||[]).push([[443],{7381:function(n,t,e){e.r(t),e.d(t,{default:function(){return l}});var u=e(3396);const r={class:"about"},s=(0,u._)("h1",null,"This is an about page",-1),a=[s];function o(n,t){return(0,u.wg)(),(0,u.iD)("div",r,a)}var c=e(89);const f={},i=(0,c.Z)(f,[["render",o]]);var l=i}}]);
+//# sourceMappingURL=about.3c133e75.js.map
